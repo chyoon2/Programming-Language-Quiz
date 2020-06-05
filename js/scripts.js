e1524de793d3044acf7a1f8b592d14d23b0102ec
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 10:33:37 -0700

    Declare variables and assign them values from the input form

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,18 @@
 
 //Interface logic
 $(document).ready(function() {
-  $("form#form-one").submit(function(event) {
+  $("form#quiz").submit(function(event) {
     event.preventDefault();
-    const spirit =  $("input:radio[name=mobile-web]:checked").val();
-    const season =  $("input:radio[name=android-ios]:checked").val();
-    const spirit =  $("input:radio[name=front-back]:checked").val();
-    const season =  $("input:radio[name=difficulty]:checked").val();
-    const season =  $("input:radio[name=logo]:checked").val();
-    
+    let androidIos =  $("input:radio[name=android-ios]:checked").val();
+    let mobileWeb =  $("input:radio[name=mobile-web]:checked").val();
+    let frontBack =  $("input:radio[name=front-back]:checked").val();
+    let difficulty =  $("input:radio[name=difficulty]:checked").val();
+    let logo =  $("input:radio[name=logo]:checked").val();
+        
+    if (androidIos === 'ios'){
+      $("#swift").show();
+    }
+    else {
+    }
   });
 });
