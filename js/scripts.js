49037fc3f6a0d618a2b3c58c7c12537df2a90f83
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 08:02:54 -0700

    Create a index.html site for a Programming Language Suggestor, a styles.css, and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Businasty logic
+
+//Interface logic
+$(document).ready(function() {
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
+    const feetInput = parseInt($("#height-feet").val());
+    
+  });
+});
