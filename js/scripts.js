25525386a4d063a719b8dc362e3f26caa2eb03fb
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 11:50:31 -0700

    Add one complete branch path

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,32 @@ $(document).ready(function() {
     let frontBack = $("input:radio[name=front-back]:checked").val();
     let difficulty = $("input:radio[name=difficulty]:checked").val();
     let logo = $("input:radio[name=logo]:checked").val();
-    $("#swift").show();
+    
+    if(androidIos === "ios"){
+      $("#swift").show();
+    } else{
+      if(mobileWeb === "mobile") {//if this is true do not show javascript
+        if (difficulty != "easy"){ //do not show python
+          if(logo === "birds"){
+            $("#swift").show();
+          } else if (logo ==="musical") {
+            $("#c").show();
+          }
+        } else {
+          if (logo === "snakes") {
+          $("#python").show();
+          } else if (logo === "birds"){
+            $("#swift").show();
+          } else if (logo === "musical"){
+            $("#c").show();
+          }
+        }
+      }
+      } 
+      //else if (mobileWeb === web) { if this true do not show swift
+
+      // }
+
+      // }
   });
 });
