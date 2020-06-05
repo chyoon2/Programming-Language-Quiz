08965ea75c34d2b308335bcc78e9917a91a081da
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 14:52:58 -0700

    Add a function holding the business logic and passing back the programming logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,27 @@
-//Businasty logic
-
 //Interface logic
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    event.preventDefault();
+    
     let androidIos = $("input:radio[name=android-ios]:checked").val();
     let mobileWeb = $("input:radio[name=mobile-web]:checked").val();
     let frontBack = $("input:radio[name=front-back]:checked").val();
     let difficulty = $("input:radio[name=difficulty]:checked").val();
     let logo = $("input:radio[name=logo]:checked").val();
     
-    if(androidIos === "android") {
-      if(mobileWeb === "mobile") { //if this is true do not show javascript
-        if (difficulty != "easy") { //do not show python/javascript/swift)
-            $("#c").show();
-        } 
-        else {
-          if (logo === "snakes" || logo === "coffee") {
-            $("#python").show();
-          } 
-          else {
-            $("#c").show();
-          } 
-        }
-      } 
-      else { //showing javascript-web
-        if (frontBack === "back") { //no longer js
-          if (difficulty != "easy") { //no python
-            $("#c").show();
+    let cJs;
+    let pythonJs;
+    let swiftJs;
+    let javascriptJs;
+    
+    businessLogic();
+    
+    $(".person1").text(businessLogic);
+
+    function businessLogic () {
+      if(androidIos === "android") {
+        if(mobileWeb === "mobile") {
+          if (difficulty != "easy") {
+              cJs = "C#"; 
           } 
           else {
             if (logo === "snakes" || logo === "coffee") {
@@ -38,31 +32,47 @@ $(document).ready(function() {
             } 
           }
         } 
-        else { //showing js
-          if (difficulty != "easy") { //no python
-            if (logo === "coffee" || logo === "snakes") { //no python
-              $("#javascript").show();
+        else { 
+          if (frontBack === "back") { 
+            if (difficulty != "easy") { 
+              $("#c").show();
             } 
             else {
-              $("#c").show();
+              if (logo === "snakes" || logo === "coffee") {
+                $("#python").show();
+              } 
+              else {
+                $("#c").show();
+              } 
             }
           } 
-          else { //js c python
-            if (logo === "coffee") {
-              $("#javascript").show();
+          else {
+            if (difficulty != "easy") { 
+              if (logo === "coffee" || logo === "snakes") { 
+                $("#javascript").show();
+              } 
+              else {
+                $("#c").show();
+              }
             } 
-            else if (logo === "snakes") {
-              $("#python").show();
-            } 
-            else if (logo === "musical") {
-              $("#c").show();
+            else { 
+              if (logo === "coffee") {
+                $("#javascript").show();
+              } 
+              else if (logo === "snakes") {
+                $("#python").show();
+              } 
+              else if (logo === "musical") {
+                $("#c").show();
+              }
             }
           }
         }
+      } 
+      else {
+        $("#swift").show();
       }
-    } 
-    else {
-      $("#swift").show();
-    }
+    } //this is the end of the function
+    event.preventDefault();
   });
 });
