823bfb083f9f0ed1a137639475b48bb624c0cf77
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 15:18:06 -0700

    Add bootstrap styling to the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,73 @@
 //Interface logic
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    const androidIos = $("input:radio[name=android-ios]:checked").val();
+    const mobileWeb = $("input:radio[name=mobile-web]:checked").val();
+    const frontBack = $("input:radio[name=front-back]:checked").val();
+    const difficulty = $("input:radio[name=difficulty]:checked").val();
+    const logo = $("input:radio[name=logo]:checked").val();
     
-    let androidIos = $("input:radio[name=android-ios]:checked").val();
-    let mobileWeb = $("input:radio[name=mobile-web]:checked").val();
-    let frontBack = $("input:radio[name=front-back]:checked").val();
-    let difficulty = $("input:radio[name=difficulty]:checked").val();
-    let logo = $("input:radio[name=logo]:checked").val();
-    
-    let cJs;
-    let pythonJs;
-    let swiftJs;
-    let javascriptJs;
+    let codingLanguage;
     
     businessLogic();
-    
-    $(".person1").text(businessLogic);
+    $(".language").text(codingLanguage);
 
     function businessLogic () {
       if(androidIos === "android") {
         if(mobileWeb === "mobile") {
           if (difficulty != "easy") {
-              cJs = "C#"; 
+            codingLanguage = "C#"; 
           } 
           else {
             if (logo === "snakes" || logo === "coffee") {
-              $("#python").show();
+              codingLanguage = "Python";
             } 
             else {
-              $("#c").show();
+              codingLanguage = "C#";
             } 
           }
         } 
         else { 
           if (frontBack === "back") { 
             if (difficulty != "easy") { 
-              $("#c").show();
+              codingLanguage = "C#";
             } 
             else {
               if (logo === "snakes" || logo === "coffee") {
-                $("#python").show();
+                codingLanguage = "Python";
               } 
               else {
-                $("#c").show();
+                codingLanguage = "C#";
               } 
             }
           } 
           else {
             if (difficulty != "easy") { 
               if (logo === "coffee" || logo === "snakes") { 
-                $("#javascript").show();
+                codingLanguage = "JavaScript";
               } 
               else {
-                $("#c").show();
+                codingLanguage = "C#";
               }
             } 
             else { 
               if (logo === "coffee") {
-                $("#javascript").show();
+                codingLanguage = "JavaScript";
               } 
               else if (logo === "snakes") {
-                $("#python").show();
+                codingLanguage = "Python";
               } 
               else if (logo === "musical") {
-                $("#c").show();
+                codingLanguage = "C#";
               }
             }
           }
         }
       } 
       else {
-        $("#swift").show();
+        codingLanguage = "Swift";
       }
-    } //this is the end of the function
-    event.preventDefault();
+    }
   });
 });
