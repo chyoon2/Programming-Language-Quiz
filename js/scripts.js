225c754a3d8c1f872a13d673832ba5d5c3030ce2
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 13:53:59 -0700

    Complete force coding of branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -10,39 +10,57 @@ $(document).ready(function() {
     let difficulty = $("input:radio[name=difficulty]:checked").val();
     let logo = $("input:radio[name=logo]:checked").val();
     
-    if(androidIos === "android"){//if this is true do not show swift
+    if(androidIos === "android") {
       if(mobileWeb === "mobile") {//if this is true do not show javascript
-        if (difficulty != "easy"){ //do not show python/javascript/swift)
+        if (difficulty != "easy") { //do not show python/javascript/swift)
             $("#c").show();
-          } else {
+          } 
+          else {
             if (logo === "snakes" || logo === "coffee") {
-            $("#python").show();
-            } else if (logo === "musical" || logo === "birds"){
+              $("#python").show();
+            } 
+            else {
               $("#c").show();
             } 
           }
-      } else if (mobileWeb === "web"){ //showing javascript
-          if (frontBack === "back") { //no longer js
-            if (difficulty !=easy){ //no python
-              $("#c").show();
-            } else if (){ //with python
-              if (logo === "snakes" || logo === "coffee") {
-                $("#python").show();
-                } else if (logo === "musical" || logo === "birds"){
-                  $("#c").show();
-                } 
-            }
-          }else if (frontBack === "front"){ //showing js
-            if (difficulty !=easy){ //no python
-
-            } else if (){ //js c python
-
-            }
-
+      } 
+      else if (mobileWeb === "web") { //showing javascript-web
+        if (frontBack === "back") { //no longer js
+          if (difficulty != "easy") { //no python
+            $("#c").show();
+          } 
+          else if (difficulty === "easy"){ //with python
+            if (logo === "snakes" || logo === "coffee") {
+              $("#python").show();
+              } 
+              else if (logo === "musical" || logo === "birds"){
+                $("#c").show();
+              } 
           }
-
+        }else if (frontBack === "front"){ //showing js
+          if (difficulty != "easy"){ //no python
+            if (logo === "coffee" || logo === "snakes"){ //no python
+              $("#javascript").show();
+            } 
+            else if (logo === "musical" || logo === "birds") {
+              $("#c").show();
+            }
+          } 
+          else if (difficulty === "easy"){ //js c python
+            if (logo === "coffee"){
+              $("#javascript").show();
+            } 
+            else if (logo === "snakes"){
+              $("#python").show();
+            } 
+            else if (logo === "musical"){
+              $("#c").show();
+            }
+          }
+        }
       }
-    } else{
+    } 
+    else{
       $("#swift").show();
     }
   });
