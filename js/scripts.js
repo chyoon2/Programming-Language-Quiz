1bf7ba145e6ee5398f4cc0f7890a20e7b05d9779
Chris Yoon <chy.yoon@gmail.com>
2020-06-05 11:01:57 -0700

    fixed bugs in .html

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,11 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    let androidIos =  $("input:radio[name=android-ios]:checked").val();
-    let mobileWeb =  $("input:radio[name=mobile-web]:checked").val();
-    let frontBack =  $("input:radio[name=front-back]:checked").val();
-    let difficulty =  $("input:radio[name=difficulty]:checked").val();
-    let logo =  $("input:radio[name=logo]:checked").val();
-        
-    if (androidIos === 'ios'){
-      $("#swift").show();
-    }
-    else {
-    }
+    let androidIos = $("input:radio[name=android-ios]:checked").val();
+    let mobileWeb = $("input:radio[name=mobile-web]:checked").val();
+    let frontBack = $("input:radio[name=front-back]:checked").val();
+    let difficulty = $("input:radio[name=difficulty]:checked").val();
+    let logo = $("input:radio[name=logo]:checked").val();
+    $("#swift").show();
   });
 });
